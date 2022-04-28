604761f7988da420be000f062fcf7440253ded16
NagaVenkatesh <nagavenkatesh.paritala.jukshio.com>
2022-04-28 19:49:24 +0530

    first commit

---
+++ app/src/main/java/com/example/jcameraapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.jcameraapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button, button1;
+    EditText widtheditText, heighteditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button1 = findViewById(R.id.imgcapture);
+        widtheditText = findViewById(R.id.widthedittxt);
+        heighteditText = findViewById(R.id.heightedittxt);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (TextUtils.isEmpty(widtheditText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Fileds can't be empty", Toast.LENGTH_SHORT).show();
+
+                } else if (TextUtils.isEmpty(heighteditText.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Fileds can't be empty", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    CameraXLibrary.camcapture(Integer.parseInt(widtheditText.getText().toString()), Integer.parseInt(heighteditText.getText().toString()), getApplicationContext(), new CaptureCameraX() {
+                        @Override
+                        public void cmeraxpreview(int width, int height) {
+
+                        }
+                    });
+                }
+
+            }
+
+        });
+
+    }
+}
